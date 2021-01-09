2108db7cf0d4626ccfc61c4e6563a60b419950a0
Mansur Emin Kaya <kayamansur61@gmail.com>
2021-01-09 15:07:29 +0300

    Initial Commit

---
+++ AppLogistMulakat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLogistMulakat
+//
+//  Created by Mansur Emin  Kaya on 9.01.2021.
+//  Copyright © 2021 Mansur Emin  Kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
